c279b7f328da0b4b4ca80bc36ab1a25b0b1a40c9
Matsushin <shinichi.matsuda@matsushin.tech>
2023-02-06 20:32:39 +0900

    デバッグテスト

--- app/controllers/health_check_controller.rb
+++ app/controllers/health_check_controller.rb
@@ -3,6 +3,9 @@ class HealthCheckController < ApplicationController
   def index
     # user_count = User.count
     # render plain: "User count: #{user_count}"
-    render plain: "OK!"
+    Rails::logger::debug('=== index ===')
+    Rails::logger::debug(ENV.fetch('MATSUHUB_TEST'))
+    Rails::logger::debug(ENV.fetch('MATSUHUB_TEST2'))
+    render plain: "OK! MATSUHUB_TEST=#{ENV.fetch('MATSUHUB_TEST')}; MATSUHUB_TEST2=#{ENV.fetch('MATSUHUB_TEST2')}"
   end
 end
