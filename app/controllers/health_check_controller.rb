8cfb179badee9616b7442924eafef9e4a8a8a60e
Matsushin <shinichi.matsuda@matsushin.tech>
2023-02-27 12:57:52 +0900

    ステージング用の環境設定を追加

--- app/controllers/health_check_controller.rb
+++ app/controllers/health_check_controller.rb
@@ -1,11 +1,7 @@
 class HealthCheckController < ApplicationController
 
   def index
-    # user_count = User.count
-    # render plain: "User count: #{user_count}"
-    Rails::logger::debug('=== index ===')
-    Rails::logger::debug(ENV.fetch('MATSUHUB_TEST'))
-    Rails::logger::debug(ENV.fetch('MATSUHUB_TEST2'))
-    render plain: "OK! MATSUHUB_TEST=#{ENV.fetch('MATSUHUB_TEST')}; MATSUHUB_TEST2=#{ENV.fetch('MATSUHUB_TEST2')}"
+    user_count = User.count
+    render plain: "User count: #{user_count}"
   end
 end
